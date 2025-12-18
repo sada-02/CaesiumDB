2b8afb6a2c91a9534e2fa69caf3d0bc0547865ad
sada-02 <kartiksarda.2005@gmail.com>
2025-12-18 19:04:05 +0530

    INFO

--- src/main.cpp
+++ src/main.cpp
@@ -788,7 +788,7 @@ string generateResponse(vector<string>& tokens , bool& sendResponse , int currFD
 string handleINFO(bool isREP=true) {
   string response = "";
   if(isREP) {
-    response+="$11\r\nrole:" + (isMaster ? "master" : "slave") + "\r\n";
+    response+="$11\r\nrole:" + string(isMaster ? "master" : "slave") + "\r\n";
   }
 
   return response;
