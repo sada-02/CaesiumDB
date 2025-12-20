663c488f1dcb73c11c85703e97f07464d4d18455
sada-02 <kartiksarda.2005@gmail.com>
2025-12-20 14:54:41 +0530

    Channel

--- src/main.cpp
+++ src/main.cpp
@@ -911,6 +911,7 @@ string generateResponse(vector<string>& tokens , bool& sendResponse , int currFD
 
     for(int i=0 ;i<tokens.size() ;i++) {
       response+="$"+to_string(int(tokens[i].size()))+"\r\n"+tokens[i]+"\r\n";
+      if(tokens[i] != "subscribe")
       channels.insert(tokens[i]);
     }
 
